23c8c2ae16ea094311ee9377bfef73279332493b
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-25 00:15:39 -0500

    Add new Promise for the loadMovies function

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,29 +1,31 @@
 let page = 1;
 
 const loadMovies = async () => {
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`);
-
-    // if response is ok
-    if (response.status === 200) {
-      const data = await response.json();
-
-      let movies = '';
-      data.results.forEach((movie) => {
-        movies += `
-          <div class="movie">
-              <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
-              <h4 class="title">${movie.title}</h4>
-          </div>
-        `;
-      });
-
-      document.getElementById('movies-page').innerHTML = movies;
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`);
+  
+      // if response is ok
+      if (response.status === 200) {
+        const data = await response.json();
+  
+        let movies = '';
+        data.results.forEach((movie) => {
+          movies += `
+            <div class="movie">
+                <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
+                <h4 class="title">${movie.title}</h4>
+            </div>
+          `;
+        });
+  
+        document.getElementById('movies-page').innerHTML = movies;
+        resolve();
+      }
+    } catch (error) {
+      reject(error);
     }
-  } catch (error) {
-    return error;
-  }
-  return;
+  });
 };
 
 const previousButton = document.getElementById('previous');
