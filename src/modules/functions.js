44179759e6232e8474a25ee3d1fe1f3389e9efee
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-25 00:37:33 -0500

    Add changes in the loadMovies function

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,30 +1,32 @@
 let page = 1;
 
 const loadMovies = () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`);
-
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`)
+      .then(response => {
       // if response is ok
-      if (response.status === 200) {
-        const data = await response.json();
-
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('Failed to load movies');
+        }
+      })
+      .then(data => {
         let movies = '';
         data.results.forEach((movie) => {
           movies += `
             <div class="movie">
-                <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
-                <h4 class="title">${movie.title}</h4>
+              <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
+              <h4 class="title">${movie.title}</h4>
             </div>
           `;
         });
-
         document.getElementById('movies-page').innerHTML = movies;
         resolve();
-      }
-    } catch (error) {
-      reject(error);
-    }
+      })
+      .catch(error => {
+        reject(error);
+      });
   });
 };
 
