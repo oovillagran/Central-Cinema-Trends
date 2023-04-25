36a99c415e4dc0b806ca0f83c54f8ad8c225e7a2
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-25 00:52:53 -0500

    Add corrections to fix ESLint report

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,34 +1,30 @@
 let page = 1;
 
-const loadMovies = () => {
-  return new Promise((resolve, reject) => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`)
-      .then(response => {
-      // if response is ok
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw new Error('Failed to load movies');
-        }
-      })
-      .then(data => {
-        let movies = '';
-        data.results.forEach((movie) => {
-          movies += `
-            <div class="movie">
-              <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
-              <h4 class="title">${movie.title}</h4>
-            </div>
-          `;
-        });
-        document.getElementById('movies-page').innerHTML = movies;
-        resolve();
-      })
-      .catch(error => {
-        reject(error);
+const loadMovies = () => new Promise((resolve, reject) => {
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`)
+    .then((response) => {
+    // if response is ok
+      if (response.status === 200) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      let movies = '';
+      data.results.forEach((movie) => {
+        movies += `
+          <div class="movie">
+            <img class="poster" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">    
+            <h4 class="title">${movie.title}</h4>
+          </div>
+        `;
       });
-  });
-};
+      document.getElementById('movies-page').innerHTML = movies;
+      resolve();
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
 
 const previousButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
