f25fd29a2fa3e0996ba4517ce74d66c804ac444b
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-26 11:37:46 -0500

    Add countElements.js

---
+++ src/modules/countElements.js
@@ -0,0 +1,17 @@
+let page = 1;
+
+const countElementsOnPage = async () => {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`)
+    const data = await response.json();
+    const moviesCount = data.results.length;
+    const countElements = document.getElementById('movies-count');
+    countElements.textContent = `(${moviesCount})`;
+
+  } catch(error) {
+    console.log(error);
+  }
+  
+}
+
+export default countElementsOnPage;
