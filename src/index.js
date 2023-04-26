55feee6438b22633ef250929d10023e52b97c7d7
Oscar Villagran <oscar.villagran@hotmail.com>
2023-04-26 12:22:47 -0500

    Add changes on CountElements.js to meet requirements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './style.css';
 import loadMovies from './modules/functions.js';
 import countElementsOnPage from './modules/countElements.js';
 
-loadMovies();
-countElementsOnPage();
-
+loadMovies().then(() => {
+  countElementsOnPage();
+});
--- src/modules/countElements.js
+++ src/modules/countElements.js
@@ -1,16 +1,8 @@
-let page = 1;
-
-const countElementsOnPage = async () => {
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f1d1819ff16bbb7177fc5279dc3acaef&page=${page}`)
-    const data = await response.json();
-    const moviesCount = data.results.length;
-    const countElements = document.getElementById('movies-count');
-    countElements.textContent = `(${moviesCount})`;
-
-  } catch(error) {
-    throw new Error(error);
-  }
+const countElementsOnPage = () => {
+  const movieElements = document.querySelectorAll('.movie');
+  const count = movieElements.length;
+  const moviesCountElement = document.getElementById('movies-count');
+  moviesCountElement.textContent = `(${count})`;
 }
 
 export default countElementsOnPage;
